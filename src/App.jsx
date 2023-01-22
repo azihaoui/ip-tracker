62d83ea7722bba45f61f1f292d2f66d376f6b31c
Rusman Steele <azihaoui@hotmail.com>
2023-01-22 21:16:05 +0100

    add zygimaticus

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Images from './assets/images/pattern-bg.png'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+        <img src={Images} className='pattern' alt="" />
+        <h1 className='ip-address'>IP Address Tracker</h1>
+        <input type="text" name="" id="" />
+    </div>
+  )
+}
+
+export default App
