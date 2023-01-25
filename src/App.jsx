9ea084d02485ff98053d1a546c4b5d9a5fa55ea5
Rusman Steele <azihaoui@hotmail.com>
2023-01-25 14:48:48 +0100

    added box

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Images from './assets/images/pattern-bg.png'
+import Arrow from './assets/images/icon-arrow.svg'
 
 function App() {
   
@@ -7,12 +8,16 @@ function App() {
   return (
     <div className="App">
         <div className='pattern'>
-            <h1 className='ip-address'>IP Address Tracker</h1>
+            <h2 className='ip-address'>IP Address Tracker</h2>
             <div className='input'>
                 <input className='input-ip-address' type="text" name="" id="" />
-                <div className='input-button'></div>
+                <div className='input-button'>
+                    <img className='arrow' src={Arrow} alt="" />
+                </div>
             </div>
-
+            <div className='box'>
+                
+            </div>
         </div>
        
     </div>
