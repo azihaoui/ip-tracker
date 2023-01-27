790f14cc13b4865582e2d848d36e3089e0876e81
Rusman Steele <azihaoui@hotmail.com>
2023-01-27 17:15:25 +0100

    added divs in the box

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
-import './App.css'
-import Images from './assets/images/pattern-bg.png'
-import Arrow from './assets/images/icon-arrow.svg'
+import "./App.css";
+import Images from "./assets/images/pattern-bg.png";
+import Arrow from "./assets/images/icon-arrow.svg";
 
 function App() {
-  
-
   return (
     <div className="App">
-        <div className='pattern'>
-            <h2 className='ip-address'>IP Address Tracker</h2>
-            <div className='input'>
-                <input className='input-ip-address' type="text" name="" id="" />
-                <div className='input-button'>
-                    <img className='arrow' src={Arrow} alt="" />
-                </div>
-            </div>
-            <div className='box'>
-                
-            </div>
+      <div className="pattern">
+        <h2 className="ip-address">IP Address Tracker</h2>
+        <div className="input">
+          <input className="input-ip-address" type="text" name="" id="" />
+          <div className="input-button">
+            <img className="arrow" src={Arrow} alt="" />
+          </div>
         </div>
-       
+        <div className="box">
+          <div className="box-section">
+            <h5>IP ADDRESS</h5>
+            <p>123.536.869.763</p>
+          </div>
+          <div className="divider"></div>
+
+          <div className="box-section">
+            <h5>LOCATION</h5>
+            <p>123.536.869.763</p>
+          </div>
+          <div className="divider"></div>
+
+          <div className="box-section">
+            <h5>TIMEZONE</h5>
+            <p>123.536.869.763</p>
+          </div>
+          <div className="divider"></div>
+
+          <div className="box-section">
+            <h5>ISP</h5>
+            <p>123.536.869.763</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
