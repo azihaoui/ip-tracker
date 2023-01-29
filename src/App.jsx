816ca15eba81d93f280bfd7f38147926a8c3c8d8
Rusman Steele <azihaoui@hotmail.com>
2023-01-29 19:32:10 +0100

    added lines

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,25 @@ function App() {
         <div className="box">
           <div className="box-section">
             <h5>IP ADDRESS</h5>
-            <p>123.536.869.763</p>
+            <p>192.212.174.101</p>
           </div>
           <div className="divider"></div>
 
           <div className="box-section">
             <h5>LOCATION</h5>
-            <p>123.536.869.763</p>
+            <p>Brooklyn, NY 10001</p>
           </div>
           <div className="divider"></div>
 
           <div className="box-section">
             <h5>TIMEZONE</h5>
-            <p>123.536.869.763</p>
+            <p>UTC -05:00</p>
           </div>
           <div className="divider"></div>
 
           <div className="box-section">
             <h5>ISP</h5>
-            <p>123.536.869.763</p>
+            <p>SpaceX Starlink</p>
           </div>
         </div>
       </div>
